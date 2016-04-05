501f111020915ba03c5318187196b470535717c9
StevenLA11717 <StevenLA11717.github@redwood6.com>
2016-04-04 22:36:52 -0400

    fixed search for show-cards

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -6,7 +6,7 @@ const Landing = () => (
     <div className='home-info'>
       <h1 className='title'>svideo</h1>
       <input className='search' type='text' placeholder='Search' />
-      <Link to='/searjjch' className='browse-all'> or Browse All</Link>
+      <Link to='/search' className='browse-all'> or Browse All</Link>
     </div>
   </div>
 )
