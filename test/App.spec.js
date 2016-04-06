dbcbe203372fb9b35f48e753ec5dd6c6c7f0ed1f
StevenLA11717 <StevenLA11717.github@redwood6.com>
2016-04-06 17:33:03 -0400

    chai enzyme shallow mount tests

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,29 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
-
+const React = require('react')
+const Search = require('../js/Search')
+const ShowCard = require('../js/ShowCard')
+const { shallow, mount } = require('enzyme')
+const { shows } = require('../public/data')
 describe('<Search />', () => {
   it('should pass', () => {
-    expect(1 + 1).to.equal(2)
+    const wrapper = shallow(<Search />)
+  // console.log(wrapper.debug())
+    expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+  })
+
+  it('should render mulitple ShowCards; one per Data', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+  })
+
+  it('should filter state correctly', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
   })
 })
