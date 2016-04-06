99a1abd3d9af6d7ee99f7bbd9283ddd8623ca940
StevenLA11717 <StevenLA11717.github@redwood6.com>
2016-04-05 22:15:46 -0400

    Created onChange Eventhandler and filtered shows

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,18 +2,48 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-const Search = () => (
-  <div className='container'>
-    <header className='header'>
-      <h1 className='brand'></h1>
-      <input type='text' className='search-input' placeholder='Search'/>
-    </header>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
-
+class Search extends React.Component {
+  /*
+    const Search = React.createClass({
+      getInitialState(){
+        return {
+          searchTerm: 'Inital State'
+        }
+      },
+      this.handleSearchTermEvent (event) {...}
+      render { return ( <div> ... </div )}
+  */
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+  }
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value})
+  }
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input value={this.state.searchTerm} type='text' className='search-input' placeholder='Search' onChange={this.handleSearchTermEvent}/>
+        </header>
+        <div className='shows'>
+          {data.shows
+            .filter((show) =>
+              `${show.title} ${show.description}`.toUpperCase()
+              .includes(this.state.searchTerm.toUpperCase())
+            )
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID}/>
+              )
+            )
+          }
+        </div>
+      </div>
+    )
+  }
+}
 module.exports = Search
