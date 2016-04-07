253ad17bb6159179aa1968ad6ccf266f953bc1f0
StevenLA11717 <StevenLA11717.github@redwood6.com>
2016-04-07 11:49:33 -0400

    Save Search.jsx using ES6 class

---
+++ js/Search-using-class.jsx
@@ -0,0 +1,58 @@
+const React = require('react')
+const ShowCard = require('./ShowCard')
+const { arrayOf, object } = React.PropTypes
+
+class Search extends React.Component {
+  /*
+    const Search = React.createClass({
+      getInitialState () {
+        return {
+          searchTerm: 'Inital State'
+        }
+      }, //PROPTYPES FOR CREATECLASS() VERSION
+      propTypes: {
+        shows: arrayOf(object)
+      },
+      handleSearchTermEvent (event) {
+        this.setState({ searchTerm: event.target.value})
+      },
+      render { return ( <div> ... </div )}
+  */
+  constructor (props) {
+    super(props)
+    console.log(props)
+    this.state = {
+      searchTerm: ''
+    }
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+  }
+
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value})
+  }
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input value={this.state.searchTerm} type='text' className='search-input' placeholder='Search' onChange={this.handleSearchTermEvent}/>
+        </header>
+        <div className='shows'>
+          {this.props.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase()
+            .includes(this.state.searchTerm.toUpperCase()))
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID}/>
+              )
+            )
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+Search.propTypes = {
+  shows: arrayOf(object)
+}
+module.exports = Search
