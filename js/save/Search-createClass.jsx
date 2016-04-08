6017129cbb25d66fd083e46b3fb2492d9f414e3f
StevenLA11717 <StevenLA11717.github@redwood6.com>
2016-04-08 15:40:10 -0400

    Save createClass Version

---
+++ js/save/Search-createClass.jsx
@@ -0,0 +1,37 @@
+const React = require('react')
+const ShowCard = require('./ShowCard')
+const { object } = React.PropTypes
+
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  propTypes: {
+    route: object
+  },
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value})
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input value={this.state.searchTerm} type='text' className='search-input' placeholder='Search' onChange={this.handleSearchTermEvent}/>
+        </header>
+        <div className='shows'>
+          {this.props.route.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().includes(this.state.searchTerm.toUpperCase()))
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID}/>
+              )
+            )
+          }
+        </div>
+      </div>
+    )
+  }
+})
+module.exports = Search
