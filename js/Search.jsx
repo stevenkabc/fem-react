61a2aebadcd54b74eefe43e4f01e9c2e3baa0bc5
StevenLA11717 <StevenLA11717.github@redwood6.com>
2016-04-11 12:17:30 -0400

    Added Redux state to Search

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,24 +4,26 @@ const Header = require('./Header')
 const { object, string } = React.PropTypes
 const { connector } = require('./Store')
 
-const Search = () => (
-  <div className='container'>
-    <Header showSearch/>
-    <div className='shows'>
-      {this.props.route.shows
-        .filter((show) => `${show.title} ${show.description}`.toUpperCase()
-        .includes(this.props.searchTerm.toUpperCase()))
-        .map((show) => (
-          <ShowCard {...show} key={show.imdbID}/>
-          )
-        )
-      }
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  propTypes: {
+    route: object,
+    searchTerm: string
+  },
+  render () {
+    return (
+      <div className='container'>
+        <Header showSearch/>
+        <div className='shows'>
+          {this.props.route.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase()
+            .includes(this.props.searchTerm.toUpperCase()))
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID}/>
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
-Search.propTypes = {
-  route: object,
-  searchTerm: string
-}
 module.exports = connector(Search)
