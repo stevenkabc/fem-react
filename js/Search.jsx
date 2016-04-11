cd1a472dd9a2925a8944f86bb177fbbb5cc93208
StevenLA11717 <StevenLA11717.github@redwood6.com>
2016-04-11 00:21:23 -0400

    Search converted to Stateless

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,40 +4,21 @@ const Header = require('./Header')
 const { object, string } = React.PropTypes
 const { connector } = require('./Store')
 
-class Search extends React.Component {
-  /*
-    const Search = React.createClass({
-      getInitialState () {
-        return {
-          searchTerm: 'Inital State'
-        }
-      }, //PROPTYPES FOR CREATECLASS() VERSION
-      propTypes: {
-        shows: arrayOf(object)
-      },
-      handleSearchTermChange (searchTerm) {
-        this.setState({ searchTerm: searchTerm })
-      },
-      render { return ( <div> ... </div )}
-  */
-  render () {
-    return (
-      <div className='container'>
-        <Header showSearch/>
-        <div className='shows'>
-          {this.props.route.shows
-            .filter((show) => `${show.title} ${show.description}`.toUpperCase()
-            .includes(this.props.searchTerm.toUpperCase()))
-            .map((show) => (
-              <ShowCard {...show} key={show.imdbID}/>
-              )
-            )
-          }
-        </div>
-      </div>
-    )
-  }
-}
+const Search = () => (
+  <div className='container'>
+    <Header showSearch/>
+    <div className='shows'>
+      {this.props.route.shows
+        .filter((show) => `${show.title} ${show.description}`.toUpperCase()
+        .includes(this.props.searchTerm.toUpperCase()))
+        .map((show) => (
+          <ShowCard {...show} key={show.imdbID}/>
+          )
+        )
+      }
+    </div>
+  </div>
+)
 
 Search.propTypes = {
   route: object,
