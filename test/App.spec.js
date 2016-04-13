06ddb36913a502557c077513f992e2f3f95953ae
StevenLA11717 <StevenLA11717.github@redwood6.com>
2016-04-12 21:55:13 -0400

    Added Redux Devtools hooks to store \nRedux Mocha Tests

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,12 +4,14 @@ const { expect } = require('chai')
 const React = require('react')
 const Search = require('../js/Search')
 const ShowCard = require('../js/ShowCard')
+const { store, rootReducer } = require('../js/Store')
 const { shallow, mount } = require('enzyme')
 const { shows } = require('../public/data')
-describe('<Search />', () => {
+
+xdescribe('<Search />', () => {
   it('should pass', () => {
     const wrapper = shallow(<Search />)
-  // console.log(wrapper.debug())
+  // c onsole.log(wrapper.debug())
     expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
   })
 
@@ -25,5 +27,16 @@ describe('<Search />', () => {
     input.simulate('change')
     expect(wrapper.state('searchTerm')).to.equal('house')
     expect(wrapper.find('.show-card').length).to.equal(2)
+  })
+})
+
+describe('Store', () => {
+  it('should bootstrap', () => {
+    const state = rootReducer(undefined, {type: '@@redux/INIT'})
+    expect(state).to.deep.equal({searchTerm: ''})
+  })
+  it('should handle setSearchTerm action', () => {
+    const state = rootReducer({searchTerm: 'some state'}, { type: 'setSearchTerm', value: 'correct string' })
+    expect(state).to.deep.equal({searchTerm: 'correct string'})
   })
 })
